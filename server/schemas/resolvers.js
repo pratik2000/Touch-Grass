0d7e1a2c95bc549fdce009d43767aba8e19a97d8
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-23 08:24:50 -0400

    took out alot of stuff in this file since the only info that will be stored is the user's info

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Profile } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    }
+  },
+  Mutation: {
+    addProfile: async (parent, { name, email, password }) => {
+      const profile = await Profile.create({ name, email, password });
+      const token = signToken(profile);
+      return { token, profile };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+      if (!profile) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+      const correctPw = await profile.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+      const token = signToken(profile);
+      return { token, profile };
+    }
+
+  }
+};
+
+module.exports = resolvers;
