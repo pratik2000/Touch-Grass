0e29dae427669ba49fcf6237f2f69e23fad359af
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-23 09:12:27 -0400

    added auth folder

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,9 @@ import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@ap
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
+import signup from "./pages/signup";
 import Home from "./pages/Home";
-//import Auth from '../src/utils/auth';
+import Auth from '../src/utils/auth';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -36,59 +36,34 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-/*
-function App() {
-  let url = 'https://www.gofundme.com/f/touchgrass?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1';
-
-  return (<Router>
-    <div className="App">
-      <Nav />
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/Home' component={Home} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
-          <Footer/>
-          
-          
-        </div>
-      </div>
-    </div></Router>
-  );
-}*/
 
 function App() {
-  const [user, setUser] = useState(false)
 
-  const handleLogin = e => {
-    e.preventDefault();
-    setUser(true);
-  }
+return (
+<ApolloProvider client={client}>
+    <Router>
+        <div className="container-fluid">
 
-  const handleLogout = e => {
-    e.preventDefault();
-    setUser(false);
-  }
+          <div className="container-fluid">
+            <Route exact path="/login">
+              <Home />
+            </Route>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+            <Route exact path="/profiles/:username">
 
-  return (
-    <div className="App">
-      <Router>
-      <Nav />
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-        <Route exact path='/' handleLogin={handleLogin} render={
-          props => <Login {...props} user={user.toString()}
-            handleLogin={handleLogin} />} />
-        <ProtectedRoute exact path='/login' user={user} handleLogout={handleLogout} component={Login} />
-        <Route exact path='/unauthorized' component={Unauthorized} />
+            </Route>
+          </div>
+          <Footer />
         </div>
-    </div>
-      </Router>
-  </div>
-  );
+    </Router>
+    </ApolloProvider>
+
+);
 }
 
 export default App;
