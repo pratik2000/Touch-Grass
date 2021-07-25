2fa7735051fdfc0e8294132831a38f037dee4b4d
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-25 15:33:46 -0400

    minor change to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
-
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/touch-grass', {
+  
+  
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
 
-module.exports = mongoose.connection;
+ 
+}).then(
+  () => {console.log("connection sucessful!");}
+).catch((e) => {
+  console.log("no connection");
+} 
+  
+);
